c67109f8fb15de3cc7ceb2ac4ea186ccba064317
Ven <ven.wu@baifu-tech.net>
2023-12-28 16:05:40 +0800

    Add Example Project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Ven on 2023/12/21.
+//
+
+import LZUIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items: [String] = [
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "LZUIKit範例"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let className = "Example\(items[indexPath.row])"
+        let controllerItem = ControllerItem(controller: className)
+        if let viewController = controllerItem.viewController {
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+        else {
+            print("Unknow class name => \(className)")
+        }
+    }
+}
+
