3eebc9cde76430b5c31d310cb4b5557fe81cc81b
Ven <ven.wu@baifu-tech.net>
2025-04-23 18:19:30 +0800

    [Fix] Loading 層級改為 -2

--- Sources/LZUIKit/UIWindow/UIWindow+LoadingLevel.swift
+++ Sources/LZUIKit/UIWindow/UIWindow+LoadingLevel.swift
@@ -3,7 +3,7 @@ import UIKit
 public extension UIWindow {
     static var loadingLevelWindow: UIWindow {
         let window = UIWindow()
-        window.windowLevel = .alert - 1
+        window.windowLevel = .alert - 2
         window.frame = UIScreen.main.bounds
         window.backgroundColor = .clear
         window.isHidden = false
